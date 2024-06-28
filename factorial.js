6f003bda900d2d935d45d5ecf1b9c615388acd79
GromovBoris <drunkgonzo@gmail.com>
2024-06-28 21:42:32 +0300

    fix: add iteractive var

--- factorial.js
+++ factorial.js
@@ -1,8 +1,19 @@
-const factorial = (n) => {
+const factorialR = (n) => {
   if (n === 1) {
     return 1;
   }
-  return n * factorial(n - 1);
+  return n * factorialR(n - 1);
 };
 
-console.log(factorial(10));
+console.log(factorialR(10));
+
+function factorialI(n) {
+  let result = 1;
+  while (n != 1) {
+    result *= n;
+    n -= 1;
+  }
+  return result;
+}
+
+console.log(factorialI(4));
